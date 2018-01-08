0f3eb7500c8e254b173afd4353b5db1f0076c20f
Thomas Pouget-Abadie <thomas.pouget-abadie@ensimag.grenoble-inp.fr>
2018-01-08 23:41:13 +0100

    generating code for readFloat() instruction

--- src/syntax/AST/ReadFloat.cpp
+++ src/syntax/AST/ReadFloat.cpp
@@ -9,3 +9,29 @@ void ReadFloat::Display(string tab)
 {
     cout << tab << ">[READFLOAT]" << endl;
 }
+
+AbstractType * ReadFloat::VerifyExpr(
+    EnvironmentType * env_types,
+    EnvironmentExp * env_exp,
+    string * class_name)
+{
+    m_expr_type = new FloatType();
+    return m_expr_type;
+}
+
+
+void ReadFloat::CodeGenExpr(
+    EnvironmentType * env_types,
+    GeneratorEnvironment * gen_env)
+{
+    gen_env->output_file << "    ; reading float from the console" << endl;
+    gen_env->output_file
+        << "invokestatic java/lang/System.console()Ljava/io/Console;"
+        << endl;
+    gen_env->output_file
+        << "invokevirtual java/io/Console.readLine()Ljava/lang/String;"
+        << endl;
+    gen_env->output_file
+        << "invokestatic  java/lang/Float.parseFloat(Ljava/lang/String;)F"
+        << endl;
+}
--- src/syntax/AST/ReadFloat.h
+++ src/syntax/AST/ReadFloat.h
@@ -10,6 +10,17 @@ class ReadFloat : public AbstractReadExpr
         AbstractExpr * Clone();
         
         void Display(string tab);
+
+        AbstractType * VerifyExpr(
+            EnvironmentType * env_types,
+            EnvironmentExp * env_exp,
+            string * class_name
+        );
+
+        void CodeGenExpr(
+            EnvironmentType * env_types,
+            GeneratorEnvironment * gen_env
+        );
 };
 
 #endif
