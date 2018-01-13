48dc3d0e56d293c65b535882eed5506a1d038397
Thomas Pouget-Abadie <thomas.pouget-abadie@ensimag.grenoble-inp.fr>
2018-01-13 18:41:33 +0100

    generating code for This

--- src/syntax/AST/This.cpp
+++ src/syntax/AST/This.cpp
@@ -20,10 +20,13 @@ AbstractType * This::VerifyExpr(
     EnvironmentExp * env_exp,
     string * class_name)
 {
-    throw runtime_error("NOT IMPLEMENTED : VERIFY This");
+    m_expr_type = env_types->GetType(*class_name);
+    return m_expr_type;
 }
 
 void This::CodeGenExpr(
     EnvironmentType * env_types,
     GeneratorEnvironment * gen_env)
-{}
+{
+    gen_env->output_file << "    aload_0" << endl;
+}
